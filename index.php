e49706c82e4736162f629774c2d032a30bd0c179
alaminrahman <sumonrahman3600@gmail.com>
2024-08-13 12:30:44 +0600

    CRUD done with PDO

--- index.php
+++ index.php
@@ -40,16 +40,19 @@ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
  $author = "Admin";
 
 # Positinal Params
-//  $sql = 'SELECT * FROM posts WHERE author = ?';
-//  $stmt = $pdo->prepare($sql);
-//  $stmt->execute([$author]);
-//  $posts = $stmt->fetchAll();
+    //  $sql = 'SELECT * FROM posts WHERE author = ?';
+    //  $stmt = $pdo->prepare($sql);
+    //  $stmt->execute([$author]);
+    //  $posts = $stmt->fetchAll();
 
 # Named Params
-    // $sql = 'SELECT * FROM posts WHERE author = :author';
+    // $sql = 'SELECT * FROM posts';
     // $stmt = $pdo->prepare($sql);
-    // $stmt->execute(['author' => $author]);
+    // $stmt->execute();
     // $posts = $stmt->fetchAll();
+
+    // echo "<pre>";
+    // var_dump($posts);
 
 # FETCH SINGLE POST
     // $sql = 'SELECT * FROM posts WHERE id = :id';
@@ -57,8 +60,55 @@ $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     // $stmt->execute(['id' => 1]);
     // $posts = $stmt->fetch();
 
-    echo "<pre>";
-var_dump($posts); 
-//  foreach($posts as $post){
-//     echo $post->title . '<br/>' ;
-//  }
+# GET ROW COUNT
+    // $stmt = $pdo->prepare('SELECT * FROM posts WHERE author = ?');
+    // $stmt->execute([$author]);
+    // $postCount = $stmt->rowCount();
+
+    // echo $postCount;
+
+# INSERT DATA
+// $title = 'Post six';
+// $body = 'This is post five';
+// $author = 'Kevin';
+
+// try {
+
+//     $sql = 'INSERT INTO posts(title, body, author) VALUES (:title, :body, :author)';
+//     $stmt = $pdo->prepare($sql);
+//     $stmt->execute(['title' => $title, 'body' => $body, 'author' => $author]);
+//     echo 'Post Added';
+
+// } catch (Exception $e) {
+//     die("Post not added: " . $e->getMessage());
+// }
+
+# UPDATE DATA
+// $id = 1;
+// $title = 'Post Update';
+// $body = 'Updated This is post five';
+
+// try {
+
+//     $sql = 'UPDATE posts SET body = :body WHERE id = :id';
+//     $stmt = $pdo->prepare($sql);
+//     $stmt->execute(['id' => $id, 'body' => $body]);
+//     echo 'Post Updated';
+
+// } catch (Exception $e) {
+//     die("Post not Updated: " . $e->getMessage());
+// }
+
+# DELETE DATA
+// $id = 5;
+
+// try {
+
+//     $sql = 'DELETE FROM posts WHERE id = :id';
+//     $stmt = $pdo->prepare($sql);
+//     $stmt->execute(['id' => $id]);
+//     echo 'Post Deleted';
+
+// } catch (Exception $e) {
+//     die("Post not Deleted: " . $e->getMessage());
+// }
